2cf718f352889f128bd0e24445549d815c205f61
Subhiksha B <subhiksha132002@gmail.com>
2025-10-05 15:46:43 +0530

    Completed GET and POST request for Products

--- OnlineRetailAPI/Controllers/ProductsController.cs
+++ OnlineRetailAPI/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OnlineRetailAPI.Data;
+using OnlineRetailAPI.Models;
+using OnlineRetailAPI.Models.Entities;
 
 namespace OnlineRetailAPI.Controllers
 {
@@ -8,14 +10,35 @@ namespace OnlineRetailAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        public ProductsController(ApplicationDbContext)
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductsController(ApplicationDbContext dbContext)
         {
-            
+            this.dbContext = dbContext;
         }
         [HttpGet]
         public IActionResult GetAllProducts()
         {
-             
+            var allProducts = dbContext.Products.ToList();
+
+            return Ok(allProducts);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(AddProductDto addProductDto)
+        {
+            var productEntity = new Product()
+            {
+                ProductName = addProductDto.ProductName,
+                ProductDescription = addProductDto.ProductDescription,
+                ProductPrice = addProductDto.ProductPrice,
+                StockQuantity = addProductDto.StockQuantity,
+                ImageUrl = addProductDto.ImageUrl
+            };
+            dbContext.Products.Add(productEntity);
+            dbContext.SaveChanges();
+
+            return Ok(productEntity);
         }
     }
 }
---
+++ OnlineRetailAPI/Models/AddProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineRetailAPI.Models
+{
+    public class AddProductDto
+    {
+        public required string ProductName { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public required decimal ProductPrice { get; set; }
+
+        public required int StockQuantity { get; set; }
+
+        public required string ImageUrl { get; set; }
+    }
+}
